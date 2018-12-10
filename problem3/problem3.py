1edcfedd0f33ea86dca76427284104d6edea3bf4
stephalexandra <44372966+stephalexandra@users.noreply.github.com>
2018-12-10 11:28:21 -0500

    Add files via upload

---
+++ problem3/problem3.py
@@ -0,0 +1,9 @@
+print ('-' * 65)
+print ('100th Birthday Program: ')
+print ('This program asks you for your current age and tells you the year that you will turn 100. ')
+x = input ('What is your age today? ')
+x = int(x)
+total = (2018 - x) + 100
+total = str(total)
+print ('You will turn 100 in the year ' + total + '!')
+print ('-' * 65)
